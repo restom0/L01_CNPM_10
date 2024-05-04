4b96b4d9171bdeb4064d727c0af78a782d29a454
zakkart54 <101053818+zakkart54@users.noreply.github.com>
2024-05-04 17:48:39 +0700

    update logout

--- client/src/layouts/Sidebar.jsx
+++ client/src/layouts/Sidebar.jsx
@@ -4,7 +4,11 @@ import { Link, useLocation } from 'react-router-dom'
 import { HiHome, HiChartBar, HiUser, HiLogout} from 'react-icons/hi'
 import { IoBuild } from 'react-icons/io5'
 import { HiDocument } from 'react-icons/hi2'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
+const cookies = new Cookies();
 const DASHBOARD_SIDEBAR_LINKS = [
 	{
 		key: 'dashboard',
@@ -57,6 +61,15 @@ const linkClass =
 	'flex items-center gap-2 px-3 py-2 hover:bg-lightGray hover:no-underline rounded-[12px] text-base font-semibold'
 
 export default function Sidebar() {
+	const navigate = useNavigate();
+	const handleLogout = async () => { 
+        try {
+            cookies.remove('sessionID');
+            navigate('/login');
+        }catch (error) {
+            alert(error);
+        }
+    };
 	return (
 		<div className="bg-white w-[260px] pt-[30px] px-[5px] flex flex-col">
 			<div className="flex flex-1 flex-col gap-0.5">
@@ -66,7 +79,7 @@ export default function Sidebar() {
 			</div>
 			
 			<div className="flex flex-col gap-0.5 pt-2 rounded-md">
-				<button type='button' className={classNames(linkClass, 'cursor-pointer text-mainRed mb-2 hover:bg-lightRed ')}>
+				<button type='button' className={classNames(linkClass, 'cursor-pointer text-mainRed mb-2 hover:bg-lightRed ')} onClick={handleLogout}>
 					<span className="text-xl">
 						<HiLogout />
 					</span>
